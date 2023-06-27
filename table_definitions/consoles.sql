e894a78b01fd2668353b0411dda7f2c59acfcf19
exholalia <me@echo-ld.com>
2023-06-27 13:52:25 +1000

    file rename

--- table_definitions/consoles.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE platforms (
-  console_id integer not null primary key,
-  name text not null
-)
-
-INSERT INTO consoles ("Steam");
-INSERT INTO consoles (167, "Playstation 5");
-INSERT INTO consoles (130, "Nintendo Switch");
-INSERT INTO consoles (14, "Mac");
---
+++ table_definitions/platforms.sql
@@ -0,0 +1,9 @@
+CREATE TABLE platforms (
+  console_id integer not null primary key,
+  name text not null
+)
+
+/* INSERT INTO platforms ("Steam"); */
+INSERT INTO platforms (167, "Playstation 5");
+INSERT INTO platforms (130, "Nintendo Switch");
+INSERT INTO platforms (14, "Mac");
