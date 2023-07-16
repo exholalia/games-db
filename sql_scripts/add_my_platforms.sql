f9270753526ef94a50f59f041466f04a832a6174
exholalia <me@echo-ld.com>
2023-07-16 11:06:59 +1000

    SQL script to add list of platforms I own

---
+++ sql_scripts/add_my_platforms.sql
@@ -0,0 +1,11 @@
+UPDATE platforms
+SET owned = 1
+WHERE console_id = 6 
+    OR console_id = 14
+    OR console_id = 39
+    OR console_id = 130
+    OR console_id = 162
+    OR console_id = 163
+    OR console_id = 167
+    OR console_id = 384
+    OR console_id = 385;
